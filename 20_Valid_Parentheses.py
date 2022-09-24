662b0b9774f184462a3e8573838a9ffec91732ef
Hadi Keramati <37190613+hkeramat@users.noreply.github.com>
2022-09-24 14:19:15 -0400

    Update 20_Valid_Parentheses.py

--- 20_Valid_Parentheses.py
+++ 20_Valid_Parentheses.py
@@ -1,22 +1,15 @@
-dict = {
+class Solution(object):
+    def isValid(self,s): 
+        dict = {
     '(': ')', '{': '}', '[' :']',
 }
-
-s1 = "]"
-
-def isValid(s):
-    open_par = []
-
-    for char in s:
-        if char in dict:
-            open_par.append(char)
-        elif open_par and dict[open_par[-1]] == char:
-            open_par.pop()
-        else:
-            return False
-    if open_par:
-        return False
-    return True
-
-
-print(isValid(s1))
+        open_par = []
+        for char in s:
+            if char in dict:
+                open_par.append(char)
+            elif open_par and dict[open_par[-1]] == char:
+                open_par.pop()
+            else:
+                return False
+        
+        return not open_par
